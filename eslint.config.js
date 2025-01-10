8aa613ab406c2d5801cfd6106dd0b2ff7145806b
Bagus Valentino <bagusvalentino.dev@gmail.com>
2025-01-10 23:53:57 +0700

    🔧 Add ESLint setup for code quality and linting

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import preferArrowFunctionsPlugin from 'eslint-plugin-prefer-arrow-functions'
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    plugins: {
+      'prefer-arrow-functions': preferArrowFunctionsPlugin
+    },
+    rules: {
+      'semi': [
+        'error',
+        'never'
+      ],
+      'comma-dangle': [
+        'error',
+        'never'
+      ],
+      'indent': [
+        'error',
+        2
+      ],
+      'object-curly-spacing': [
+        'error',
+        'always'
+      ],
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'prefer-arrow-functions/prefer-arrow-functions': [
+        'error',
+        {
+          'allowNamedFunctions': false,
+          'classPropertiesAllowed': false,
+          'disallowPrototype': false,
+          'returnStyle': 'unchanged',
+          'singleReturnOnly': false
+        }
+      ]
+    }
+  },
+  pluginJs.configs.recommended
+]
